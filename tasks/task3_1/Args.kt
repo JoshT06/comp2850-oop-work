96d64ad5a516b95bb21a3034a685d04fe711c1a9
Joshua T <Twixter06@hotmail.com>
2025-10-07 11:53:25 +0000

    yay

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,10 @@
 // Task 3.1: command line arguments
+
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    println(args[0])
+    println(args[1])
+    }
+
+    // required argument available as args[0]
---
+++ tasks/task4_2/Pizza.kt
@@ -0,0 +1,9 @@
+fun main(){
+
+    println("PIZZA MENU \n \n (a) Margherita \n (b) Quattro Stagioni \n (c) Seafood \n (d) Hawaiian \n \n Choose your pizza (a-d):")
+
+    val userChoice = readln().lowercase()
+    if (userChoice.length > 1)
+
+
+}
