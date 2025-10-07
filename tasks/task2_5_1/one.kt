c462a8462b453aade800034489669ee1fe31c5fa
Joshua T <Twixter06@hotmail.com>
2025-10-07 00:13:46 +0000

    Up to task 2 complete

---
+++ tasks/task2_5_1/one.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var x = 5;
+    println (x);
+    x = 6;
+    println (x);
+}
---
+++ tasks/task2_5_2/two.kt
@@ -0,0 +1,21 @@
+fun main() {
+val myAge = 29u
+val universeAge = 13_800_000_000L
+val status = 'M'
+val name = "Sarah"
+val height = 1.78f
+val root2 = Math.sqrt(2.0)
+
+println(myAge);
+println(myAge::class);
+println(universeAge);
+println(universeAge::class);
+println(status);
+println(status::class);
+println(name)
+println(name::class)
+println(height)
+println(height::class)
+println(root2)
+println(root2::class)
+}
